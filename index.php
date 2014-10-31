c5fb4ecc44e5596d92c3700a2b49cc213d323780
Alexandre DeFreitas <abd9@njit.edu>
2014-10-31 18:52:14 -0400

    added API keys and tokens to index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-
-  echo "<h2>Twitter API Test</h2>";
+  require_once('TwitterAPIExchange.php');
+  
+  $settings = array(
+    'oauth_access_token' => "2849321127-VTfYJfhIobqLa1CUKSMDaQwv9tRs9bRB0uWGLdq",
+    'oauth_access_token_secret' => "fMmAUgvf0w3RbFDMYrgaBHTucR9Otom5IsDk3nW5W5vCW",
+    'consumer_key' => "cR6YV752XhP8lgvcfEwcQUO4y",
+    'consumer_secret' => "4ZEV91gYD9wxUbprqeUX9lQcaea1IxDu74EAKUBFhRz1nJ1z7L"
+  );
 
 ?>
